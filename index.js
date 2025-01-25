bf9cbde3d7986b62d464be0ee50da44a6f30f726
MarcellKov <kmarcell379@gmail.com>
2025-01-25 18:28:58 +0100

    Socket.io logikájának megírása, minimélis UI kialakítás, leaderboard sorbarendezési taktikák

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import {renderFile} from "ejs"
 import {Server} from "socket.io"
 import { createServer } from 'node:http';
 import {config} from "dotenv"
-
+import cors from "cors"
+import ratelimiter from "express-rate-limit"
 
 
 
@@ -16,8 +17,12 @@ const app=express()
 const MainServer=createServer(app)
 const io=new Server(MainServer)
 
-app.use(express.json())
 
+app.use(express.json())
+app.use("/read_book",ratelimiter({
+    windowMs:1000*5*100,
+    limit:1000
+}))
 app.set('view engine', "ejs")
 app.engine('html', renderFile);
 
@@ -38,26 +43,49 @@ const RTDB=getDatabase(FirebaseApp)
 
 
 onChildChanged(query(ref(RTDB,"leaderboard")),(snapshot)=>{
-    
     console.log(snapshot.toJSON())
     console.log(snapshot.ref.key)
 })
 
-app.get("/",(req,res)=>{
-    res.render("index.html",{"title":["a","b"]})
+app.get("/",async (req,res)=>{
+    var processed=new Map()
+
+    var data=(await get(query(ref(RTDB,"leaderboard")))).forEach(function(e){
+        processed.set(e.key,e.val()["num"])
+    })
+    
+    console.log(processed)
+    
+    const sortedEntries = Array.from(processed).sort((a, b) => {
+        console.log(a,b)
+        return b[1] - a[1]
+      });
+      console.log(sortedEntries)
+      const sortedMap = new Map(sortedEntries)
+        
+    res.render("index.html",{"title":sortedMap})
     return
 })
 
 app.post("/read_book",(req,res)=>{
     let id=req.body["id"]
     
-    update(ref(RTDB,`leaderboard/${id}`),{"num":increment(1)})
+    update(ref(RTDB,`leaderboard/${id}`),{"num":increment(1),})
+    io.emit("change",id)
     res.send("Success")
     return
+})
+
+app.post("/new_reader",(req,res)=>{
+    let id=req.body["id"]
+    set(ref(RTDB,`leaderboard/${id}`),{"num":0})
+    io.emit("newID",id)
 })
 
 io.on("connection",(socket)=>{
     console.log("Joined")
 })
+
+
 
 MainServer.listen(5000)
